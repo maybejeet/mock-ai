7cce8b545842772e2d00ea8a298f6503ba3e8fe6
maybejeet <contactjeet247@gmail.com>
2026-01-17 23:31:26 +0530

    first commit

---
+++ proxy.ts
@@ -0,0 +1,34 @@
+import { clerkMiddleware , createRouteMatcher } from '@clerk/nextjs/server';
+import {  NextResponse } from 'next/server'; 
+
+const isProtectedRoute = createRouteMatcher(["/dashboard"])
+const publicRoutes = createRouteMatcher(["/", "/api/webhook/register", "/sign-in(.*)", "/sign-in(.*)"]);
+
+// export default clerkMiddleware()
+
+export default clerkMiddleware(async (auth, req) => {
+    const { isAuthenticated } = await auth()
+
+    try {
+        if (!isAuthenticated && isProtectedRoute(req)) {
+            return NextResponse.redirect(new URL("/sign-in", req.url))
+        }
+        if(isAuthenticated && publicRoutes(req)){
+            return NextResponse.redirect(new URL("/dashboard", req.url))
+        }
+    } catch (error) {
+        console.error("Error fetching user data from Clerk:", error);
+        return NextResponse.redirect(new URL("/error", req.url));
+    }
+})
+
+// export default clerkMiddleware();
+
+export const config = {
+    matcher: [
+    // Skip Next.js internals and all static files, unless found in search params
+    '/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)',
+    // Always run for API routes
+    '/(api|trpc)(.*)',
+    ],
+};
